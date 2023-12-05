ced999bf706b578bf22ca6fbf43edbeaf526d809
Yannick Denis <1509yadenis@student.epfc.eu>
2023-12-05 15:14:40 +0100

    feat(login):login ok

--- controller/ControllerTest.php
+++ controller/ControllerTest.php
@@ -4,7 +4,9 @@ require_once "framework/Controller.php";
 class ControllerTest extends Controller {
     public function index() : void {
         if ($this->user_logged()) {
-            $this->redirect("test");
+            $user=  $this->get_user_or_redirect();
+            (new View("test"))->show();
+            
         } else {
             (new View("login"))->show(["pseudo"=>"","password"=>"","errors"=>""]);
         }        
