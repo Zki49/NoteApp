86a9b57e86554483010fcc321e4cfa66295ea665
Margaux Faure <margauxfaure@hotmail.com>
2023-12-08 13:17:35 +0100

    feat(settings): debut du dev

---
+++ view/view_settings.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Settings</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="settings-container">
+        <h1>Settings</h1>
+        
+        <ul class="settings-list">
+            <li><a href="#" id="logoutLink">Logout</a></li>
+            <li><a href="#" id="editProfileLink">Edit profile</a></li>
+            <li><a href="#" id="changePasswordLink">Change password</a></li>
+        </ul>
+    </div>
+</body>
+</html>
