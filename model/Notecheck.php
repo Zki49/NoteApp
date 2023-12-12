cfa5dc7b3e87fd1217955c92736568e9010feab3
Yannick Denis <1509yadenis@student.epfc.eu>
2023-12-12 19:29:41 +0100

    feat(model note):debut du dev model de notes check

---
+++ model/Notecheck.php
@@ -0,0 +1,30 @@
+<?php
+class Notecheck extends Note{
+
+    // a complete demain 
+    public function __construct( $title,$owner, $createat,$editedat, bool $pinned, bool $archived,int $weight,private string $content  ) {
+        parent::__construct($title,$owner,$createat,$editedat,$pinned,$archived,$weight);
+    }
+    public static function get_note_by_id(int $id): Notecheck |false{
+        $query = self::execute("SELECT * FROM check_listnotes nt ,notes n where n.id= :id and nt.id = n.id", ["id"=>$id] );
+        $data = $query->fetch(); // un seul résultat au maximum
+        if ($query->rowCount() == 0) { 
+            return false;
+        } else {
+            return new Notecheck($data["title"],User::get_user_by_id($data["owner"]),$data["created_at"],$data["edited_at"],$data["pinned"],
+                                $data["archived"],$data["weight"],$data["content"]);
+        }
+        
+    }
+    public static function get_notes_by_user(User $user): Notecheck |false {
+        $query = self::execute("SELECT * FROM check_listnotes nt ,notes n where n.owner= :idowner and nt.id = n.id", ["idowner"=>$user->get_id()] );
+        $data = $query->fetch(); // un seul résultat au maximum
+        if ($query->rowCount() == 0) { 
+            return false;
+        } else {
+            return new Notecheck($data["title"],User::get_user_by_id($data["owner"]),$data["created_at"],$data["edited_at"],$data["pinned"],
+                                $data["archived"],$data["weight"],$data["content"]);
+        }
+    } 
+}
+?>
