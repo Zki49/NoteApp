101b429e54e7d2cafa036cf60befbbdef700c54a
Yannick Denis <1509yadenis@student.epfc.eu>
2023-12-21 22:55:47 +0100

    feat(viewnote):corection de la methode shared by

--- controller/ControllerTest.php
+++ controller/ControllerTest.php
@@ -6,7 +6,7 @@ class ControllerTest extends Controller {
     public function index() : void {
         if ($this->user_logged()) {
             $user=  $this->get_user_or_redirect();
-            $tab_shared = User::array_shared_user_by_mail($user->getMail());
+            $tab_shared = User::array_shared_user_by_mail($user);
 
             (new View("test"))->show(["user"=>$user, "tab_shared" =>$tab_shared] );
             
