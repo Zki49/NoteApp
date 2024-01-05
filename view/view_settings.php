b26d8d0c02ea1fe2c69ccadd6cac177383131a08
Zakaria Guendouzi <guendouzi2003@gmail.com>
2024-01-05 18:43:49 +0100

    feat(view) : modif de view

--- view/view_settings.php
+++ view/view_settings.php
@@ -16,8 +16,8 @@
 <div class="container mt-4">
     <div class="d-flex justify-content-between align-items-center">
 
-        <button class="btn btn-light">
-            <a href="notes" id="btnRetour">
+        <button class="btn">
+            <a href="notes" id="btnRetour" style="color: white; text-decoration: none;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                 </svg>
