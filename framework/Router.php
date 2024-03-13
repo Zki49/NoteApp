3b34a7b9e6ed768f12a90fe673d4ac47d28a0006
Yannick Denis <1509yadenis@student.epfc.eu>
2024-03-13 13:35:44 +0100

    fix(framworck):patch

--- framework/Router.php
+++ framework/Router.php
@@ -13,7 +13,7 @@ class Router
             if (is_array($value)) {
                 $copy[$key] = $this->sanitize_all_array($value);
             } else {
-                if(Configuration::get("insafe_inputs") && !in_array($key, Configuration::get("insafe_inputs")))
+                if(!Configuration::get("insafe_inputs") || (Configuration::get("insafe_inputs") && !in_array($key,  Configuration::get("insafe_inputs"))))
                     $copy[$key] = Tools::sanitize($value);
                 else
                     $copy[$key] = $value;
