4cbed79fdc4364abced1cacfca38504e4c73cef4
Margaux Faure <margauxfaure@hotmail.com>
2024-03-26 14:21:42 +0100

    feat(toggle): changement de reader a editor et inversement

--- view/view_shared.php
+++ view/view_shared.php
@@ -15,7 +15,8 @@
   <script>
     let idUser, selectedOptionUser, userName,
     idPermission, selectedOptionPerm, permName,
-    div, toggleBtn;
+    div, toggleBtn,
+    html;
     
 
     $(function(){
@@ -46,13 +47,13 @@
     });
 
     function viewShare(){
-      let html = '<input id="user" name="user" value="';
+      html = '<input id="user" name="user" value="';
       html += userName;
       html += " (";
       html += permName;
       html += ')"';
       html += ' readonly></input>';
-      html += '<button type="submit" class="btn btn-primary mb-2" >'
+      html += '<button type="submit" class="btn btn-primary mb-2" onclick="toggleA()" >'
       html += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">'
       html += '<path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>'
       html += '<path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>'
@@ -68,7 +69,18 @@
     }
 
     function toggleA(){
-      console.log("1111");
+      if(idPermission == 1){
+        idPermission = 0;
+        permName = " (Reader)";
+      }
+      else{
+        idPermission = 1;
+        permName = " (Editor)";
+      }
+      userName = $("#user").val().split(" ")[0];
+      $("#user").attr("value",userName + permName);
+      console.log();
+            
     }
 
     function erase(){
