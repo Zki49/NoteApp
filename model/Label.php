8b648ccac0a1b322c541d106f066dac3adee4213
Zakaria Guendouzi <guendouzi2003@gmail.com>
2024-04-30 17:10:00 +0200

    feat(label) : ajout methodes add_label,delete_label et get_all_labels_by_user

--- model/Label.php
+++ model/Label.php
@@ -22,6 +22,34 @@
         }
     }
 
+    public static function  get_all_labels_by_user(int $id):array|bool{
+     $query = self::execute("SELECT DISTINCT label 
+                              FROM note_labels nl 
+                              WHERE nl.note in (SELECT n.id
+                                             FROM notes n
+                                             WHERE n.owner = :idUser)
+                              ORDER BY label ASC",["idUser"=>$id]);
+
+       $data = $query->fetchAll();
+       if ($query->rowCount() == 0) { 
+         return false;
+          } else {
+          $results = [];
+          foreach ($data as $row) {
+               $results[] = new Label(0,$row["label"]);
+          }
+          return $results;
+          }
+     }
+
+     public function delete_label(int $idnote, string $label){
+          self::execute("DELETE FROM note_labels WHERE note = :idnote and label = :label",["idnote" => $idnote,"label" => $label]);
+     }
+
+     public function add_label(int $idnote,string $label){    
+          self::execute("INSERT INTO note_labels (note,label) VALUES(:idnote,:label)",["idnote"=>$idnote,"label"=>$label]);
+     }
+
      public static function  get_labels_by_note(int $idnote):array|bool{
         $query = self::execute("SELECT * from note_labels where note = :idnote ORDER BY label ASC",["idnote"=>$idnote]);
 
