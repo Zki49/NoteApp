26f908a405389e506b4e4c2a3852c6c16da5460e
Zakaria Guendouzi <guendouzi2003@gmail.com>
2024-05-31 22:25:00 +0200

    feat(checkUncheck) : ok en php

--- view/view_opencheck.php
+++ view/view_opencheck.php
@@ -87,9 +87,18 @@
                 <a href='notes/check/";echo $items[$i]->get_id(); echo"'>";
                 if($items[$i]->item_checked()){
                     echo"
-                    <input id= 'check-"; echo $items[$i]->get_id(); echo"' class='form-check-input mt-0' type='checkbox' checked input'>";
+                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-check-square-fill' viewBox='0 0 16 16'>
+                        <path d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z'/>
+                    </svg>
+                    ";
+//                    <input id= 'check-"; echo $items[$i]->get_id(); echo"' class='form-check-input mt-0' type='checkbox' checked input'>";
+
                 }else{
-                    echo"<input id= 'check-"; echo $items[$i]->get_id(); echo"' class='form-check-input mt-0' type='checkbox' input'>";
+                    echo"
+                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-square' viewBox='0 0 16 16'>
+                        <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'/>
+                    </svg>";
+                   // echo"<input id= 'check-"; echo $items[$i]->get_id(); echo"' class='form-check-input mt-0' type='checkbox' input'>";
                 }
                 echo"
                 </a>
